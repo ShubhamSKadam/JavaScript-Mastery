3d0bc9e8b460a5575252fe2dfb4062491ad774de
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-11-14 20:45:56 +0530

    Variables in js

---
+++ 1_variables_and_data_types/Variables.js
@@ -0,0 +1,16 @@
+// JavaScript has 3 ways of declaring a variable
+// 1. Using var
+// 2. Using let
+// 3. Using const ( Read Only )
+
+// There are some rules to declare variable names
+// 1. Do not existing JS keywords
+// 2. You cannot use any special character while naming a variable other then _ and $
+
+var variableName = "Welcome to Variables";
+
+console.log(variableName);
+
+variableName = "Hello World";
+
+console.log(variableName);
