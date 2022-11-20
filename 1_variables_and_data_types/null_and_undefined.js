c1f142dcfd418a9825b21c40eb1699a1d9f01ddb
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-11-20 19:49:39 +0530

    null and undefined learnings added

---
+++ 1_variables_and_data_types/null_and_undefined.js
@@ -0,0 +1,10 @@
+// Null
+// Null is assigning a value i.e nothing
+const age = null;
+
+console.log(age);
+
+// when you don't assign any value to the variable then it becomes undefined.
+let name;
+
+console.log(typeof null);
