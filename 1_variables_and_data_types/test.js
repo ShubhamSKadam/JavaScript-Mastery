22e5e353492f683dda4564ac203ecc8651fd556c
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-11-28 19:10:42 +0530

    few edits in test.js

--- 1_variables_and_data_types/test.js
+++ 1_variables_and_data_types/test.js
@@ -1,6 +1,6 @@
 let n = 20;
 
 for (let i = 0; i < n; i++) {
-  n % i == 0 ? console.log(i) : console.log("fuck");
+  n % i == 0 ? console.log(i) : console.log("testing");
 }
 console.log(typeof undefined);
