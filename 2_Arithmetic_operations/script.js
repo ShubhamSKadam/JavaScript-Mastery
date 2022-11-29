cb2d2a13400b23dc9b0ce8a365c945311182b1c0
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-11-29 17:28:42 +0530

    Arithmetic Operations

---
+++ 2_Arithmetic_operations/script.js
@@ -0,0 +1,27 @@
+const a = 5;
+const b = 10;
+let result = 0;
+
+// Addition
+// let result = a + b;
+
+// Subtraction
+// let result = a - b;
+
+// Multiplication
+// let result = a * b;
+
+// Division
+// let result = a / b;
+
+// Exponent
+// let result = b ** a;
+
+// Modulo
+// let result = a % b;
+
+// Increment and Decrement
+result++;
+result--;
+
+console.log(result);
