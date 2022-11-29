02b2c27d03e4a7cbdcc9460d2d864a462b384d12
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-11-29 16:44:04 +0530

    Objects added

---
+++ 1_variables_and_data_types/objects.js
@@ -0,0 +1,15 @@
+// Objects
+
+const Person = {
+  name: "Shubham",
+  age: 21,
+};
+
+console.log(typeof Person);
+
+const arr = [1, 2, 3, 4];
+console.log(typeof arr);
+
+const date = new Date();
+
+console.log(typeof date);
