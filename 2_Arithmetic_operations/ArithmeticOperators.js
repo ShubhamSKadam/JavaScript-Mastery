71a86a2144e7c578f5ac5cf4e19644144a73ef79
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-12-01 19:44:07 +0530

    Comparison Operators

--- 2_Arithmetic_operations/ArithmeticOperators.js
+++ 2_Arithmetic_operations/ArithmeticOperators.js
@@ -3,22 +3,22 @@ const b = 10;
 let result = 0;
 
 // Addition
-// let result = a + b;
+result = a + b;
 
 // Subtraction
-// let result = a - b;
+result = a - b;
 
 // Multiplication
-// let result = a * b;
+result = a * b;
 
 // Division
-// let result = a / b;
+result = a / b;
 
 // Exponent
-// let result = b ** a;
+result = b ** a;
 
 // Modulo
-// let result = a % b;
+result = a % b;
 
 // Increment and Decrement
 result++;
---
+++ 2_Arithmetic_operations/comparisonOperators.js
@@ -0,0 +1,28 @@
+// Comparison Operators ==> true / false
+
+const a = 5;
+const b = 10;
+
+// Greater than
+console.log(a > b);
+
+// Greater than equal to
+console.log(a >= b);
+
+// Less than
+console.log(a < b);
+
+// Less than equal to
+console.log(a <= b);
+
+// Is equal
+console.log(a == b);
+
+// Not equal
+console.log(a != b);
+
+// Strict Equality
+console.log(a === b);
+
+// Strict Inequality
+console.log(a !== b);
