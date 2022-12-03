175e2e381a20dbba4f971b32cf59b97d56e5fb6d
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-12-03 16:02:27 +0530

    Evil Twins

--- 2_Arithmetic_operations/comparisonOperators.js
+++ 2_Arithmetic_operations/comparisonOperators.js
@@ -15,14 +15,25 @@ console.log(a < b);
 // Less than equal to
 console.log(a <= b);
 
-// Is equal
+// Loose equality
+// Doesn't compare the DATA TYPES
 console.log(a == b);
 
 // Not equal
 console.log(a != b);
 
 // Strict Equality
+// Compares VALUES and DATA TYPES
+// Returns true only if both are the same
 console.log(a === b);
 
 // Strict Inequality
 console.log(a !== b);
+
+// Logical Operators
+// AND && => ALL OPERANDS ARE TRUE => TRUE
+console.log(true && true && true);
+// OR || => ATLEAST ONE OPERAND IS TRUE => TRUE
+console.log(true || false);
+// NOT ! => Reverses the Boolean value
+console.log(!true);
