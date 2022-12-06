b62d326af0c0b92c6593261a5ad4e9d2a393b923
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-12-06 22:35:01 +0530

    Truthy and falsy

---
+++ 3_Logic_and_Control_flow/script.js
@@ -0,0 +1,31 @@
+// If condition
+
+// if (condition){
+//    block of code
+// }
+
+// const age = 8;
+
+// if (age > 18) {
+//   console.log("You may enter");
+// } else if (age === 18) {
+//   console.log("Welcome, You just turned 18!");
+// } else {
+//   console.log("Grow up");
+// }
+
+// FALSY VALUES - Excluding them all other values are TRUTHY VALUES
+// false
+// 0
+// ''
+// null
+// undefined
+// NaN
+
+// SOME TRUTHY VALUES
+// true
+// 1
+// "hello"
+// 27 (any number)
+// {} // empty object is also considered to be true
+// [] // empty array is also considered to be true
