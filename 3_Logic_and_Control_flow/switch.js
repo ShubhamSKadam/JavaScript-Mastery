c985cc346979cac32b2076334a1b401187680e28
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-12-08 10:51:48 +0530

    Switch Statement

---
+++ 3_Logic_and_Control_flow/switch.js
@@ -0,0 +1,22 @@
+const superHero = "Batman";
+
+switch (superHero) {
+  case "Captain America":
+    console.log("Never give up!");
+    break;
+
+  case "Iron Man":
+    console.log("I am Iron Man!");
+    break;
+
+  case "Thor":
+    console.log("That is my hammer!");
+    break;
+
+  case "Black Widow":
+    console.log("One shot, one kill");
+    break;
+
+  default:
+    console.log("Enter a valid superhero name");
+}
