4c8fa8ec65a411dd25294c7075ba2c1cca8b536c
ShubhamSKadam <shubhamskadam99@outlook.com>
2022-12-10 17:56:55 +0530

    Paramters and Arguments

--- 4_Functions/ArrowFunction.js
+++ 4_Functions/ArrowFunction.js
@@ -9,3 +9,17 @@ const square1 = (number) => number * number;
 
 const result = square(3);
 console.log(result);
+
+// Difference between parameters and arguments
+// Paramters => used when defining a function
+// Arguments => passed when making a function call
+// E.g.
+
+const sayHi = (name, age = 0) => {
+  // By default the we can make value of age will be 0, to avoid undefined as a result when age is not being passed.
+
+  // name is a paramter
+  console.log(`Hi ${name}, you are ${age} years old!`);
+};
+
+sayHi("Shubham"); // 'Shubham' is an argument value that is being passed
