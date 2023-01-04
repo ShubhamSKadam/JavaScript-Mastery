cd9d38081ab03098718959882fabc7d7ea7058d4
ShubhamSKadam <shubhamskadam99@outlook.com>
2023-01-04 18:09:31 +0530

    Classes

--- 10_DOM/script.js
+++ 10_DOM/script.js
@@ -1,1 +1,9 @@
+const menuClicked = (currEl) => {
+    const menuItems = document.getElementsByClassName("menu-item");
 
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].classList.remove("active");
+    }
+
+    currEl.classList.add("active");
+};
