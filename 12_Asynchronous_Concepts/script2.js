f89638fe8d73efe89f691d53a5a9f9cf4b8dc386
ShubhamSKadam <shubhamskadam99@outlook.com>
2023-01-05 18:13:49 +0530

    Promises

--- 12_Asynchronous_Concepts/script2.js
+++ 12_Asynchronous_Concepts/script2.js
@@ -1,12 +1,32 @@
-// Callback functions
+const fetchUser = (username) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("[Now we have the user]");
 
-const fetchUser = (username, callback) => {
-    console.log("Fetching...");
-    setTimeout(() => {
-        callback({ username });
-    }, 2000);
+            resolve({ username });
+        }, 2000);
+    });
 };
 
-fetchUser("Shubham", (user) => {
-    console.log(`Hello, ${user.username}`);
-});
+const fetchUserPhotos = (username) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Now we have the photos for ${username}`);
+            resolve(["Photo1", "Photo2"]);
+        }, 2000);
+    });
+};
+
+const fetchPhotoDetails = (photo) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`[Now we have the photo details ${photo}]`);
+            resolve("details...");
+        }, 2000);
+    });
+};
+
+fetchUser("Shubham")
+    .then((user) => fetchUserPhotos(user.username))
+    .then((photos) => fetchPhotoDetails(photos[0]))
+    .then((details) => console.log(`Your photo details are ${details}`));
